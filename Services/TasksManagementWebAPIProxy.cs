f701994ab6c51ca3733d95ca1a382298aa9954ef
Ofer Zadikario <ofer.zadikario@gmail.com>
2024-09-29 19:43:11 +0300

    added tunnel options code

--- Services/TasksManagementWebAPIProxy.cs
+++ Services/TasksManagementWebAPIProxy.cs
@@ -12,10 +12,27 @@ namespace TasksManagementApp.Services
 {
     public class TasksManagementWebAPIProxy
     {
+        #region without tunnel
+        /*
+        //Define the serevr IP address! (should be realIP address if you are using a device that is not running on the same machine as the server)
+        private static string serverIP = "localhost";
         private HttpClient client;
         private string baseUrl;
-        public static string BaseAddress = DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5110/api/" : "http://localhost:5110/api/";
-        private static string ImageBaseAddress = DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5110" : "http://localhost:5110";
+        public static string BaseAddress = (DeviceInfo.Platform == DevicePlatform.Android &&
+            DeviceInfo.DeviceType == DeviceType.Virtual) ? "http://10.0.2.2:5110/api/" : $"http://{serverIP}:5110/api/";
+        private static string ImageBaseAddress = (DeviceInfo.Platform == DevicePlatform.Android &&
+            DeviceInfo.DeviceType == DeviceType.Virtual) ? "http://10.0.2.2:5110" : $"http://{serverIP}:5110";
+        */
+        #endregion
+
+        #region with tunnel
+        //Define the serevr IP address! (should be realIP address if you are using a device that is not running on the same machine as the server)
+        private static string serverIP = "0jg8w9v7-5110.euw.devtunnels.ms";
+        private HttpClient client;
+        private string baseUrl;
+        public static string BaseAddress = "https://0jg8w9v7-5110.euw.devtunnels.ms/api/";
+        private static string ImageBaseAddress = "https://0jg8w9v7-5110.euw.devtunnels.ms/";
+        #endregion
 
         public TasksManagementWebAPIProxy()
         {
