17077ba66adc4ed898c3e8dc0c5a3acd384acbe9
Ofer Zadikario <ofer.zadikario@gmail.com>
2025-01-27 16:07:12 +0200

    added a distance method to the GeoCodingService class

--- Services/GeoCodingService.cs
+++ Services/GeoCodingService.cs
@@ -77,6 +77,27 @@ public class GeocodingService
         };
     }
 
+    //This method gets two pairs of latitude and longitude and returns the distance between them in kilometers
+    public static double DistanceBetweenTwoLocations(double lat1, double lon1, double lat2, double lon2)
+    {
+        var R = 6371; // Radius of the earth in km
+        var dLat = Deg2Rad(lat2 - lat1);  // deg2rad below
+        var dLon = Deg2Rad(lon2 - lon1);
+        var a =
+          Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+          Math.Cos(Deg2Rad(lat1)) * Math.Cos(Deg2Rad(lat2)) *
+          Math.Sin(dLon / 2) * Math.Sin(dLon / 2)
+          ;
+        var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+        var d = R * c; // Distance in km
+        return d;
+    }
+
+    //this method converts degrees to radians
+    private static double Deg2Rad(double deg)
+    {
+        return deg * (Math.PI / 180);
+    }
 
 }
 
